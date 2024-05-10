d86d25988ebfb1896a8948f1b44b37315f08d5c2
karxg <133251363+karxg@users.noreply.github.com>
2024-05-10 17:25:10 +0300

    Update 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -9,3 +9,12 @@ package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
+
+# Set PATH environment variable
+file_line { 'set_path':
+  path    => '/etc/environment',
+  line    => 'PATH=$PATH:/usr/local/bin',
+  match   => '^PATH=',
+  ensure  => present,
+  require => Package['Flask']
+}
